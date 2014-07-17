22d518ea9e56b8b8217ee2a971d2f0d39830f849
Marina Gourtovaia <mg8@sanger.ac.uk>
2014-07-17 17:30:08 +0100

    more realistic callbacks with tests

--- lib/npg_tracking/monitor/status.pm
+++ lib/npg_tracking/monitor/status.pm
@@ -3,11 +3,15 @@ package npg_tracking::monitor::status;
 use Moose;
 use Carp;
 use English qw(-no_match_vars);
+use Readonly;
 use Linux::Inotify2;
+use Errno qw(EINTR EIO :POSIX);
+
+use npg_tracking::util::types;
 
 our $VERSION = '0';
 
-has 'dir'        =>   (isa             => 'Str',
+has 'dir'        =>   (isa             => 'NpgTrackingDirectory',
                        is              => 'ro',
                        required        => 1,
                       );
@@ -31,46 +35,79 @@ sub _build__notifier {
   return $inotify;
 }
 
-has '_watch_obj'  =>  (isa             => 'ArrayRef',
+has '_watch_obj'  =>  (isa             => 'HashRef',
                        is              => 'ro',
                        required        => 0,
                        init_arg        => undef,
-                       default         => sub { [] },
+                       default         => sub { {} },
                       );
 
-sub _setup {
+sub _error {
+  my ($self, $path, $error) = @_;
+  $self->_cancel_watch();
+  my $m;
+  if      ($error == EBADF) {
+    $m = q[The given file descriptor is not valid.];
+  } elsif ($ERRNO == EINVAL) {
+    $m = q[The given event mask contains no legal events.];
+  } elsif ($ERRNO == ENOMEM) {
+    $m = q[Insufficient kernel memory was available.];
+  } elsif ($ERRNO == ENOSPC) {
+    $m = q[The user limit on the total number of inotify watches was reached or the kernel failed to allocate a needed resource.];
+  #} elsif ($ERRNO == EACCESS) {
+  #  $m = q[Read access to the given file is not permitted.]; #  EACCESS is not recognised
+  } else {
+    $m = q[];
+  }
+  croak "Error when trying to set watch on $path: '$error' $m";
+  return;
+}
+
+sub _runfolder_watch_setup {
+  my ($self, $name) = @_;
+  _log("runforder $name watch setup called");
+  return;
+}
+
+sub _runfolder_watch_cancel {
+  my ($self, $name) = @_;
+  _log("runforder $name watch cancel called");
+  return;
+}
+
+
+sub _watch_setup {
   my $self = shift;
 
-  push @{$self->_watch_obj},
-  $self->_notifier->watch($self->link, IN_DONT_FOLLOW | IN_IGNORED, sub {
-    my $e = shift;
-    _log('callback invoked');
-    my $name = $e->fullname;
-    if ( $e->IN_IGNORED ) {
-      _log("events for $name have been lost");
-    }
-    $e->w->cancel;
+  my $watch = $self->_notifier->watch($self->dir, IN_ISDIR | IN_UNMOUNT | IN_IGNORED | IN_MOVED_TO | IN_DELETE, sub {
+      my $e = shift;
+      my $name = $e->fullname;
+      if ($e->IN_IGNORED) {
+        croak "Events for $name have been lost";
+      }
+      if ($e->IN_UNMOUNT) {
+        croak "Filesystem unmounted for $name";
+      }
+      if ($e->IN_DELETE) {
+        _log("$name deleted");
+        $self->_runfolder_watch_cancel($name);
+      } elsif ($e->IN_MOVED_TO) {
+        _log("$name moved to the watched directory");
+        $self->_runfolder_watch_setup($name);
+      }
   });
 
-  push @{$self->_watch_obj},
-  $self->_notifier->watch($self->dir, IN_CLOSE_WRITE | IN_IGNORED, sub {
-    my $e = shift;
-    _log('callback invoked');
-    my $name = $e->fullname;
-    if ( $e->IN_CLOSE_WRITE ) {
-      _log("$name was written to");
-    } elsif ($e->IN_IGNORED) {
-      _log("events for $name have been lost");
-      $e->w->cancel;
-    }
-  });
-
-  return;
+  if (!$watch) {
+    my $err = $ERRNO;
+    $self->_error($self->dir, $err);
+  }
+  $self->_watch_obj->{$self->dir} = $watch;
+  return $watch;
 }
 
 sub watch {
   my $self = shift;
-  $self->_setup;
+  $self->_watch_setup;
   while (1) {
     my $received = $self->_notifier->poll(); #this blocks
     _log("Event count $received");
@@ -78,12 +115,14 @@ sub watch {
   return;
 }
 
-sub cancel_watch {
+sub _cancel_watch {
   my $self = shift;
-  foreach my $w (@{$self->_watch_obj}) {
-    if ($w) {
+  foreach my $key (keys %{$self->_watch_obj}) {
+    my $w = $self->_watch_obj->{$key};
+    if ($w && (ref $w eq q[Linux::Inotify2::Watch])) {
       _log('canceling watch for ' . $w->name);
       $w->cancel;
+      delete $self->_watch_obj->{$key};
     }
   }
   return;
@@ -92,14 +131,14 @@ sub cancel_watch {
 sub _log {
   my $m = shift;
   if ($m) {
-    print "$m\n" or carp "Failed to log message $m";
+    warn "$m\n";
   }
   return;
 }
 
 sub DEMOLISH {
   my $self = shift;
-  $self->cancel_watch();
+  $self->_cancel_watch();
   return;
 }
 
@@ -125,10 +164,6 @@ npg_tracking::monitor::status
  use cancel_watch method to cancel all current
  watches andrelease system resources associated
  with them.
-
-=head2 cancel_watch
-
- Cancels all known watches
 
 =head2 DEMOLISH
 
--- t/36-npg_tracking-monitor-status.t
+++ t/36-npg_tracking-monitor-status.t
@@ -1,7 +1,8 @@
 use strict;
 use warnings;
-use Test::More tests => 3;
+use Test::More tests => 12;
 use Test::Exception;
+use Test::Warn;
 use File::Temp qw/ tempdir /;
 
 use_ok( q{npg_tracking::monitor::status} );
@@ -11,5 +12,46 @@ isa_ok($m, q{npg_tracking::monitor::status});
 lives_ok { $m->_notifier } 'notifier object created';
 
 my $dir = tempdir(UNLINK => 1);
+diag $dir;
+{
+  my $m = npg_tracking::monitor::status->new(dir => $dir, link => q[t]);
+  lives_ok {$m->_watch_setup} 'watch is set up on an empty directory';
+  ok(exists $m->_watch_obj->{$dir}, 'watch object is cached');
+  is(ref $m->_watch_obj->{$dir}, q[Linux::Inotify2::Watch], 'correct object type');
+  is($m->_watch_obj->{$dir}->name, $dir, 'watch object name'); 
+  warning_like {$m->_cancel_watch} qr/canceling watch for $dir/, 'watch cancelled';
+  ok(!exists $m->_watch_obj->{$dir}, 'watch object is not cached');
+}
+
+{
+  my $m = npg_tracking::monitor::status->new(dir => $dir, link => q[t]);
+  lives_ok {$m->_watch_setup} 'watch is set up on an empty directory';
+  my $new_dir = "$dir/test";
+  mkdir $new_dir;
+  my $pid = fork();
+  if ($pid) {
+    warnings_like { $m->_notifier->poll() } 
+     [qr/$new_dir deleted/, qr/runforder $new_dir watch cancel called/ ],
+     'deletion reported';
+    wait;
+  } else {
+    rmdir $new_dir;
+    exit 0;
+  }
+ 
+  mkdir "$dir/test1";
+  mkdir "$dir/test1/test2";  
+ 
+  $pid = fork();
+  if ($pid) {
+    warnings_like { $m->_notifier->poll() } 
+     [qr/$dir\/test2 moved to the watched directory/, qr/runforder $dir\/test2 watch setup called/ ],
+     'move reported';
+    wait;
+  } else {
+    `mv $dir/test1/test2 $dir`;
+    exit 0;
+  }  
+}
 
 1;
