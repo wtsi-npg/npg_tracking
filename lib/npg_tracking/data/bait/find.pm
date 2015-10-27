2b8ebcdc17b46dce4ee1d76d85d1151452d75ebd
Steven Leonard <srl@sanger.ac.uk>
2015-10-27 10:04:16 +0000

    removed unnecessary comment

--- lib/npg_tracking/data/bait/find.pm
+++ lib/npg_tracking/data/bait/find.pm
@@ -46,9 +46,8 @@ sub _build_bait_path {
   }
   if (scalar @refs > 1) {
     ##no critic (ProhibitParensWithBuiltins)
-    ####croak 'Multiple references returned: ' . join(q[ ], @refs);
-    ##use critic
     $self->messages->push('Multiple references returned: ' . join(q[ ], @refs));
+    ##use critic
     return;
   }
   my $reference = $refs[0];
