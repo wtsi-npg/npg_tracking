3e2bc46c1ba1db3c9d5993e25bfb945be5eee82b
Keith James <dev@deoxybyte.co.uk>
2020-04-07 14:02:36 +0100

    Add --dry-run, --help options and POD
    Add warnings if default metadata values are overridden.

--- bin/npg_heron_metadata_upload.pl
+++ bin/npg_heron_metadata_upload.pl
@@ -34,7 +34,10 @@ my $verbose;
 
 GetOptions('debug'                               => \$debug,
            'dry_run|dry-run'                     => \$dry_run,
-           'id_run|id-run=@'                     => \@id_run,
+           'help'                    => sub {
+             pod2usage(-verbose => 2, -exitval => 0);
+           },
+           'id_run|id-run=i'                     => \@id_run,
            'instrument_make|instrument-make=s'   => \$instrument_make,
            'instrument_model|instrument-model=s' => \$instrument_model,
            'library_name|library-name=s'         => \$library_name,
@@ -63,6 +66,16 @@ if (not $token) {
   pod2usage(-msg => 'A --token argument is required',  -exitval => 2);
 }
 
+if ($instrument_make) {
+  $log->warn("Overriding default instrument make with '$instrument_make'");
+}
+if ($instrument_model) {
+  $log->warn("Overriding default instrument model with '$instrument_model'");
+}
+if ($library_name) {
+  $log->warn("Overriding default library name with '$library_name'");
+}
+
 my $warehouse_db = WTSI::DNAP::Warehouse::Schema->connect();
 my $tracking_db = npg_tracking::Schema->connect();
 
@@ -78,7 +91,7 @@ foreach my $id_run (@id_run) {
     # Query the tracking database to get the run. This provides two items of
     # metadata; the run folder name to use as a surrogate for "run_name" and
     # the instrument model.
-    my $run = $tracking_db->resultset('Run')->find({ id_run => $id_run });
+    my $run = $tracking_db->resultset('Run')->find({id_run => $id_run});
     if (not defined $run) {
       croak "Failed to find run with id_run $id_run\n";
     }
@@ -86,9 +99,9 @@ foreach my $id_run (@id_run) {
     # Query ML warehouse for pool identifier to use as a surrogate for
     # "library_name".Tag zero reads are those not assigned when deplexing.
     my $rs = $warehouse_db->resultset("IseqProductMetric")->search
-        ({ "me.id_run"    => $id_run,
-           "me.tag_index" => { ">" => 0 } }, # Ignore tag zero
-         { prefetch => "iseq_flowcell" });
+        ({"me.id_run"    => $id_run,
+          "me.tag_index" => {">" => 0}}, # Ignore tag zero
+         {prefetch => "iseq_flowcell"});
 
     my @pool_ids = uniq map {$_->iseq_flowcell->id_pool_lims} $rs->all;
     if (not @pool_ids) {
@@ -135,3 +148,75 @@ else {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+npg_heron_metadata_upload
+
+=head1 SYNOPSIS
+
+npg_heron_metadata_upload --id-run <id_run> [--id-run <id_run> ...]
+  --url <upload base URL>
+  --username <API user name> --token <API access token>
+  [--instrument-make <instrument override>]
+  [--instruemnt-model <instrument model override>]
+  [--library-name <library name override>]
+  [--dry-run] [--debug] [--verbose]
+
+ Options:
+   --debug            Enable debug level logging. Optional, defaults to
+                      false.
+   --dry-run
+   --dry_run          Enable dry run. Optional, defaults to false.
+   --help             Display help.
+   --id-run
+   --id_run           NPG tracking Illumina run ID. May be used more than
+                      once, to upload multiple runs.
+   --instrument-make
+   --instrument_make  Override the instrument make for all runs. Optional.
+   --instrument-model
+   --instrument_model Override the instrument model for all runs. Optional.
+   --library-name
+   --library_name     Override the library name for all runs. Optional.
+   --token            The API access token. Required.
+   --url              The API base URL e.g. https://majora.covid19.climb.ac.uk/.
+                      Required.
+   --username         The API user name. Required.
+   --verbose          Print messages while processing. Optional.
+
+=head1 DESCRIPTION
+
+This script uploads run metadata for Illumina runs to CLIMB. See
+https://docs.covid19.climb.ac.uk/
+
+The minimum information you need to use this script is an API account
+(user name and access token), plus one or more NPG Illumina run IDs.
+
+The script requires access to both the tracking database (for run
+information) and the ML warehouse (for library/pool information).
+
+While it is possible to use CLI options to set instrument make and model
+and library name, this is strongly discouraged for consistency of
+tracking.
+
+=head1 AUTHOR
+
+Keith James <kdj@sanger.ac.uk>
+
+=head1 COPYRIGHT AND DISCLAIMER
+
+Copyright (C) 2020, Genome Research Limited. All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the Perl Artistic License or the GNU General
+Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+=cut
