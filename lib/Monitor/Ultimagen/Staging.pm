4c802731478ab175bccb030e9fe4d36a43f69137
Marco Mosca <mm51@sanger.ac.uk>
2026-05-08 12:16:28 +0100

    Add UG200 monitor support

--- lib/Monitor/Ultimagen/Staging.pm
+++ lib/Monitor/Ultimagen/Staging.pm
@@ -38,7 +38,7 @@ Utilities to interrogate the staging area designated to an Ultimagen instrument.
 Find valid run folders for Ultimagen runs in a top folder (or staging area).
 A valid run folder has RunID_LibraryInfo.xml file.
 
-The path pattern matches [staging_area]/Runs/[run_folder]
+The path pattern matches .../staging/[instrument_name]/[run_folder]
 
 A list of run folder paths is returned.
 =cut
